8d65eb1a44e29ba8be118d5bad4f4b33254aab1f
jmtygsh <jmtygsh@gmail.com>
2025-08-01 23:56:47 +0530

    Fix Build Bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import { Header } from "@/components/Header";
-import { Homepage } from "@/pages/Homepage"
+import { Homepage } from "@/(pages)/Homepage"
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
